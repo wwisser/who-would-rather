77dd6db0baf0c2ca238b11ffefb241644753618d
Wisser <wisserwendelin@gmail.com>
2020-12-20 14:57:29 +0100

    Add tabbing

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -3,6 +3,43 @@ import {makeStyles} from '@material-ui/core/styles';
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
 import Paper from "@material-ui/core/Paper";
+import * as PropTypes from "prop-types";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+
+function TabPanel(props) {
+    const {children, value, index, ...other} = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 const useStyles = makeStyles(() => ({
     paper: {
@@ -12,24 +49,43 @@ const useStyles = makeStyles(() => ({
         flexWrap: 'wrap',
         padding: '10px'
     },
+    textField: {
+        paddingRight: '15px'
+    }
 }));
 
 export default function Form() {
     const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
-        <Paper class={classes.paper}>
-            <Tabs
-                value={0}
-                indicatorColor="primary"
-                textColor="primary"
-                onChange={() => console.log('187')}
-                aria-label="disabled tabs example"
-            >
-                <Tab label="Create Game"/>
-                <Tab label="Join Game"/>
-            </Tabs>
+        <div>
+            <Paper class={classes.paper}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    aria-label="disabled tabs example"
+                >
+                    <Tab label="Create Game" {...a11yProps(0)} />
+                    <Tab label="Join Game" {...a11yProps(1)} />
+                </Tabs>
 
-        </Paper>
+            </Paper>
+            <TabPanel value={value} index={0}>
+                <form>
+                    <TextField id="outlined-basic" label="Name" variant="outlined"/>
+                </form>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <TextField id="outlined-basic" label="Name" variant="outlined"/>
+                <TextField id="outlined-basic" label="Questions" type="number" variant="outlined"/>
+            </TabPanel>
+        </div>
     );
 }
